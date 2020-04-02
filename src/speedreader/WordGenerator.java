f5eca82a557a9437f9758ebabfffa1795903becb
Davin Lin <lindavin@grinnell.edu>
2020-04-02 18:43:14 -0400

    Finish WordGenerator

---
+++ src/speedreader/WordGenerator.java
@@ -0,0 +1,55 @@
+package speedreader;
+
+import java.util.Scanner;
+import java.io.File;
+
+public class WordGenerator {
+	
+	// +--------+---------------------
+	// | Fields |
+	// +--------+
+	private int wordCount = 0;
+	private int sentenceCount = 0;
+	private Scanner scr;
+	
+	// +-------------+-----------------------------------------
+	// | Constructor |
+	// +-------------+
+	
+	public WordGenerator(String filename) throws Exception{
+		this.scr = new Scanner(new File(filename));
+		
+	} // WordGenerator(String)
+	
+	// +---------+--------------------------------------------
+	// | Methods |
+	// +---------+
+	public boolean hasNext() {
+		return scr.hasNext();
+	} // hasNext()
+	
+	public String next() {
+		String next;
+		if(hasNext()) {
+			wordCount++;
+			next = scr.next();
+			if(sentenceEnd(next))
+				sentenceCount++;
+		}
+		return next();
+	} // next()
+	
+	public static boolean sentenceEnd(String word) {
+		char endChar = word.charAt(word.length() - 1);
+		return (endChar == '.') || (endChar == '?') || (endChar == '!');
+	} // sentenceEnd(String)
+	
+	public int getWordCount() {
+		return wordCount;
+	} // getWordCount()
+	
+	public int getSentenceCount() {
+		return sentenceCount;
+	} // getSentenceCount()
+	
+} // Class WordGenerator
